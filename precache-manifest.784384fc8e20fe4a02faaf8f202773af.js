a64e6f2ce6027d293d3217a4110c9280102c997e
shutsugan <muhammad@nextmedia.ma>
2019-01-24 17:05:46 +0100

    Updates

---
+++ precache-manifest.784384fc8e20fe4a02faaf8f202773af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d23e69e96b8c0fc8de3f5ffccfa062b5",
+    "url": "/resume/static/media/resume.d23e69e9.pdf"
+  },
+  {
+    "revision": "f8d962976858d4a33ff661d7bbe70c3a",
+    "url": "/resume/static/media/dev.f8d96297.mp4"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "6f44c0aca9cbe5d884f3",
+    "url": "/resume/static/js/main.6f44c0ac.chunk.js"
+  },
+  {
+    "revision": "916b373cfef80c85a28b",
+    "url": "/resume/static/js/1.916b373c.chunk.js"
+  },
+  {
+    "revision": "6f44c0aca9cbe5d884f3",
+    "url": "/resume/static/css/main.fbe2d5d7.chunk.css"
+  },
+  {
+    "revision": "bf2d534209918c7ede60b67acf3949dd",
+    "url": "/resume/index.html"
+  }
+];
